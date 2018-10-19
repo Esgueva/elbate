b80edd58e50943193ee46c73ae47abebfb0fb0c6
Rodrigo Esgueva <esgueva@gmail.com>
2018-10-19 22:44:24 +0200

    Material Design

---
+++ resources/views/master.blade.php
@@ -0,0 +1,24 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+   
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="favicon.ico">
+        
+        <title> @yield('title') </title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-material-design.min.css" rel="stylesheet" >
+        <link href="css/custom.css" rel="stylesheet">
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap-material-design.min.js"></script> 
+    </head>
+
+    <body>
+        @include('shared.navbar')
+        @yield('content')
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,58 +1,11 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-   
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="favicon.ico">
-
-        <title>Cañas de Dulzaina - El Bate</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Boostrap Core -->
-        <link href="css/bootstrap.css" rel="stylesheet">
-
-        <!-- Custom  -->
-        <link href="css/custom.css" rel="stylesheet">
-    </head>
-
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                
-                <div class="title m-b-md">
-                    Cañas de Dulzaina
-                </div>
-
-                <div class="links">
-                    <a href="/tipos">Tipos</a>
-                    <a href="/ofertas">Ofertas</a>
-                    <a href="/acerca">Acerca</a>
-                    <a href="/contacto">Contacto</a>
-                </div>
-
-            </div>
+@extends('master')
+@section('title', 'Cañas de Dulzaina')
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Cañas de Dulzaina
         </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first + Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-    <script src="js/bootstrap.min.js"></script>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
